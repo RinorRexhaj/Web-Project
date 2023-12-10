c39357be28fd039aa8be7c12e967b70f2ee4c355
DESKTOP-N7RN8SB\PC <rinori13579@gmail.com>
2023-12-10 11:32:18 +0100

    Fixed

--- home.js
+++ home.js
@@ -10,7 +10,7 @@ menu.addEventListener("click", () => {
 /* Stick the Navbar to the top of the page after scrolling */
 window.addEventListener("scroll", () => {
   let scroll = this.scrollY;
-  if (scroll > 250) {
+  if (scroll > 400) {
     header.classList.add("scroll");
   } else {
     header.classList.remove("scroll");
