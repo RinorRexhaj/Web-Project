d0afff1f28f173a2a348f7eda130a0c80527e9d4
Rinor Rexhaj <105825061+RinorRexhaj@users.noreply.github.com>
2024-01-01 23:55:01 +0100

    Deleted unnecessary files

--- contact.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
-const bars = document.querySelector(".menuBtn");
-
-menu.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
